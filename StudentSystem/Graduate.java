b768b50d0d49e66c029590e3dfbedda3828f5254
Yassine <152300833+jitting@users.noreply.github.com>
2025-04-18 16:33:07 +0100

    Create Graduate.java

---
+++ StudentSystem/Graduate.java
@@ -0,0 +1,34 @@
+package StudentSystem;
+
+public class Graduate extends Student{
+    private String thesisTopic;
+
+    public Graduate() {
+        super();
+    }
+
+    public Graduate(String name, double gpa, String thesisTopic) {
+        super(name, gpa);
+        this.thesisTopic = thesisTopic;
+    }
+
+    @Override
+    public boolean onProbation() {
+        return (this.getGPA() < 3.0);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + 
+        "Thesis Topic: " + this.thesisTopic;
+    }
+
+    public String getThesisTopic() {
+        return thesisTopic;
+    }
+    
+
+    public void setThesisTopic(String thesisTopic) {
+        this.thesisTopic = thesisTopic;
+    }   
+}
