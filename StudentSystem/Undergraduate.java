591c8b68e77d673564995d1a99b30b54591960c1
Yassine <152300833+jitting@users.noreply.github.com>
2025-04-18 16:33:21 +0100

    Create Undergraduate.java

---
+++ StudentSystem/Undergraduate.java
@@ -0,0 +1,33 @@
+package StudentSystem;
+
+public class Undergraduate extends Student{
+    private boolean isHonors;
+
+    public Undergraduate() {
+        super();
+    }
+
+    public Undergraduate(String name, double gpa, boolean isHonors) {
+        super(name, gpa);
+        this.isHonors = isHonors;
+    }
+
+    @Override
+    public boolean onProbation() {
+        return (this.getGPA() < 2.0);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + 
+        "Is an honor student: " + this.isHonors;
+    }
+
+    public boolean getHonors() {
+        return isHonors;
+    }
+
+    public void setHonors(boolean isHonors) {
+        this.isHonors = isHonors;
+    }
+}
